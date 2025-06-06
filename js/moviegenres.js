bbafbc8f081b6c3c94063c2e08d22de48dcd4d7b
AgustinFischer <afischer@udesa.edu.ar>
2025-06-06 13:29:44 -0300

    Corrijo géneros duplicados en moviegenres.htm y añado js

---
+++ js/moviegenres.js
@@ -0,0 +1,19 @@
+let apiKey = "49f77e6a13f6c95371d8c2e2d5585eee";
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=es`;
+
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    let contenedor = document.querySelector(".contenedor-generos");
+    for (let i = 0; i < data.genres.length; i++) {
+      let genero = data.genres[i];
+      contenedor.innerHTML += `
+        <a href="detail-genres.html?id=${genero.id}&type=movie" class="foto-genero">${genero.name}</a>
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
