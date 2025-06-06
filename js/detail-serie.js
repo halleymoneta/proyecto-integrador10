337a261d38f46d134c9892ad8ef8b818dc0aff06
halleymoneta <hmonetajomaa@udesa.edu.ar>
2025-06-06 15:14:10 -0300

    corregi errores en detail-serie.js y detail-serie.html

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -10,20 +10,23 @@ fetch(url)
     return response.json();
   })
   .then(function(data) {
-    console.log(data);
     document.querySelector(".titulo").innerText = data.name;
-document.querySelector(".rating").innerText = `${data.vote_average}`;
-document.querySelector(".fecha").innerText = `${data.first_air_date}`;
-document.querySelector(".sinopsis").innerText = data.overview;
-document.querySelector(".portada").src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
-document.querySelector(".portada").alt = data.name;
+    document.querySelector(".rating").innerText = `Calificación: ${data.vote_average}/10`;
+    document.querySelector(".fecha").innerText = `Fecha de estreno: ${data.first_air_date}`;
+    document.querySelector(".sinopsis").innerText = `Sinopsis: ${data.overview}`;
+    document.querySelector(".portada").src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    document.querySelector(".portada").alt = data.name;
 
-let generoContainer = document.querySelector(".generos");
-for (let genero of data.genres) {
-  generoContainer.innerHTML += `<a href="./detail-genres.html?id=${genero.id}&type=serie">${genero.name}</a> `;
-}
-
+    let generoContainer = document.querySelector(".generos");
+    generoContainer.innerHTML = "Géneros: ";
+    for (let i = 0; i < data.genres.length; i++) {
+      generoContainer.innerHTML += `<a href="./detail-genres.html?id=${data.genres[i].id}&type=serie">${data.genres[i].name}</a>`;
+      if (i !== data.genres.length - 1) {
+        generoContainer.innerHTML += " | ";
+      }
+    }
   })
   .catch(function(error) {
     console.log("Error: " + error);
+    document.querySelector("main").innerHTML = "<p>No se pudo cargar la información de la serie.</p>";
   });
