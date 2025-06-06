e3ec288a3d7a42c6395193fec1bc0d1aa5824676
AgustinFischer <afischer@udesa.edu.ar>
2025-06-06 15:17:03 -0300

    uso fetch

--- js/seriegenres.js
+++ js/seriegenres.js
@@ -1,2 +1,7 @@
 let apiKey = "49f77e6a13f6c95371d8c2e2d5585eee";
 let url = `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=es`;
+
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
